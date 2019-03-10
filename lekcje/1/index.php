7a635349230aa12b287ae51de2f8a223639ab917
Rafal <raphaelmaj@gmail.com>
2019-03-10 22:59:41 +0100

    start php

---
+++ lekcje/1/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$nazwa=3;
+$number = 3.5;
+$string  = 'string';
+$string2 = "string";
+$string_html = '<h2 style="color:#FF0;">test</h2>';
+$string_html2 = "<h2 style=\"color:#FF0;\">test</h2>";
+$bool = true; //false
+$null = null;
+
+$array = [];
+$object = new stdClass();
+
+
+
+
+
+
---
+++ lekcje/1/index2.php
@@ -0,0 +1,50 @@
+<?php
+
+$l1 = 10;
+$l2 = 22.6;
+
+$suma = $l1+$l2;
+$l1 += $l2;
+$l1 = ($l1+$l2)*2;
+
+$l = 1;
+
+//echo $l++;
+//echo '<br>';
+//echo $l;
+
+
+$str = 'Hello';
+
+$str .= ' World';
+
+$la = 'sdsdhdgs'.$str;
+
+
+//var_dump(1==1);
+//var_dump(1!=1);
+//var_dump('1'==1);
+//var_dump('1'===1);
+//var_dump('1'!==1);
+//var_dump(2>1);
+//var_dump(2>=1);
+//var_dump(2<=1);
+//var_dump(3<=>2);
+var_dump(!true);
+var_dump(!false);
+//echo $str;
+
+
+
+
+
+
+
+
+//echo $l1;
+//
+//echo $suma;
+
+// + - * / liczba**x 
+
+//echo 3%2;
