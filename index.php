ba95b71f619ee46f1f8627308c4756c018a61871
unknown <erafal.majewski@gmail.com>
2019-03-18 13:33:02 +0100

    index

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 
-echo 'test';
+<ul>
+    <li><a href="<?php echo "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>lekcje">Lekcje</a></li>
+    <li><a href="<?php echo "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>blog">Blog</a></li>
+    <li><a href="<?php echo "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>mod_rewrite">Przepisywanie adresów</a></li>
+</ul>
 
-?>
+</body>
+</html>
